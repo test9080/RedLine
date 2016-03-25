c3c53d717f0240ce1ed1e4b8e0cd2808b26923e8
chengxianghe <chengxianghe@outlook.com>
2016-03-26 02:27:13 +0800

    加入基本信息

---
+++ RedLine/Class/Global/Macro/TUUIMacro.h
@@ -0,0 +1,62 @@
+//
+//  TUUIMacro.h
+//  RedLine
+//
+//  Created by chengxianghe on 16/3/26.
+//  Copyright © 2016年 cn. All rights reserved.
+//
+
+#ifndef TUUIMacro_h
+#define TUUIMacro_h
+
+// 屏幕大小
+#define kScreenSize     [[UIScreen mainScreen] bounds].size
+
+#define kScreenWidth    [[UIScreen mainScreen] bounds].size.width
+#define kScreenHeight   [[UIScreen mainScreen] bounds].size.height
+#define kScreenOneScale (1.0 / [UIScreen mainScreen].scale)
+
+
+#define kTUNotificationCenter  [NSNotificationCenter defaultCenter]
+//#define kTUNotificationPost(name, object)  [kTUNotificationCenter postNotificationName:name object:#object]
+
+
+#define DebugLog(s, ...) NSLog(@"%s(%d): %@", __FUNCTION__, __LINE__, [NSString stringWithFormat:(s), ##__VA_ARGS__])
+
+// tip
+#define kTipAlert(_S_, ...)     [[[UIAlertView alloc] initWithTitle:@"提示" message:[NSString stringWithFormat:(_S_), ##__VA_ARGS__] delegate:nil cancelButtonTitle:@"知道了" otherButtonTitles:nil] show]
+
+#ifndef weakify
+#if DEBUG
+#if __has_feature(objc_arc)
+#define weakify(object) autoreleasepool{} __weak __typeof__(object) weak##_##object = object;
+#else
+#define weakify(object) autoreleasepool{} __block __typeof__(object) block##_##object = object;
+#endif
+#else
+#if __has_feature(objc_arc)
+#define weakify(object) try{} @finally{} {} __weak __typeof__(object) weak##_##object = object;
+#else
+#define weakify(object) try{} @finally{} {} __block __typeof__(object) block##_##object = object;
+#endif
+#endif
+#endif
+
+#ifndef strongify
+#if DEBUG
+#if __has_feature(objc_arc)
+#define strongify(object) autoreleasepool{} __typeof__(object) object = weak##_##object;
+#else
+#define strongify(object) autoreleasepool{} __typeof__(object) object = block##_##object;
+#endif
+#else
+#if __has_feature(objc_arc)
+#define strongify(object) try{} @finally{} __typeof__(object) object = weak##_##object;
+#else
+#define strongify(object) try{} @finally{} __typeof__(object) object = block##_##object;
+#endif
+#endif
+#endif
+
+
+#endif /* TUUIMacro_h */
--- RedLine/Class/Manager/TUSystemInfoManager.m
+++ RedLine/Class/Manager/TUSystemInfoManager.m
@@ -8,6 +8,125 @@
 
 #import "TUSystemInfoManager.h"
 
+@implementation BatteryInfo
+@end
+
+
+@interface TUSystemInfoManager ()
+
+@end
+
 @implementation TUSystemInfoManager
+
++ (TUSystemInfoManager *)manager {
+    static id manager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        manager = [[self alloc] init];
+        [manager configNotification];
+    });
+    return manager;
+}
+
+- (void)configNotification {
+    [self startBatteryMonitoring];
+    
+}
+
+- (BatteryInfo *)batteryInfo {
+    if (_batteryInfo == nil) {
+        _batteryInfo = [[BatteryInfo alloc] init];
+        // 此处可写默认参数
+    }
+    return _batteryInfo;
+}
+
+
+
+- (void)startBatteryMonitoring
+{
+    UIDevice *device = [UIDevice currentDevice];
+
+    if (!device.batteryMonitoringEnabled) {
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(batteryLevelUpdatedCB:)
+                                                     name:UIDeviceBatteryLevelDidChangeNotification
+                                                   object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(batteryStatusUpdatedCB:)
+                                                     name:UIDeviceBatteryStateDidChangeNotification
+                                                   object:nil];
+        
+        [device setBatteryMonitoringEnabled:YES];
+        
+        // If by any chance battery value is available - update it immediately
+        if ([device batteryState] != UIDeviceBatteryStateUnknown) {
+            [self doUpdateBatteryStatus];
+        }
+    }
+}
+
+- (void)stopBatteryMonitoring {
+    if ([UIDevice currentDevice].batteryMonitoringEnabled) {
+        [[UIDevice currentDevice] setBatteryMonitoringEnabled:NO];
+        [[NSNotificationCenter defaultCenter] removeObserver:self];
+    }
+}
+
+#pragma mark - private
+
+//- (NSUInteger)getBatteryCapacity
+//{
+//    HardcodedDeviceData *hardcode = [HardcodedDeviceData sharedDeviceData];
+//    return [hardcode getBatteryCapacity];
+//}
+//
+//- (CGFloat)getBatteryVoltage
+//{
+//    HardcodedDeviceData *hardcode = [HardcodedDeviceData sharedDeviceData];
+//    return [hardcode getBatteryVoltage];
+//}
+
+- (void)batteryLevelUpdatedCB:(NSNotification*)notification {
+    [self doUpdateBatteryStatus];
+    [kTUNotificationCenter postNotificationName:kBatteryLevelChange object:self.batteryInfo];
+}
+
+- (void)batteryStatusUpdatedCB:(NSNotification*)notification {
+    [self doUpdateBatteryStatus];
+}
+
+- (void)doUpdateBatteryStatus {
+    float batteryMultiplier = [[UIDevice currentDevice] batteryLevel];
+    self.batteryInfo.levelPercent = batteryMultiplier * 100;
+    self.batteryInfo.levelMAH =  self.batteryInfo.capacity * batteryMultiplier;
+    
+    switch ([[UIDevice currentDevice] batteryState]) {
+        case UIDeviceBatteryStateCharging:
+            // UIDeviceBatteryStateFull seems to be overwritten by UIDeviceBatteryStateCharging
+            // when charging therefore it's more reliable if we check the battery level here
+            // explicitly.
+            if (self.batteryInfo.levelPercent == 100)
+            {
+                self.batteryInfo.status = @"Fully charged";
+            }
+            else
+            {
+                self.batteryInfo.status = @"Charging";
+            }
+            break;
+        case UIDeviceBatteryStateFull:
+            self.batteryInfo.status = @"Fully charged";
+            break;
+        case UIDeviceBatteryStateUnplugged:
+            self.batteryInfo.status = @"Unplugged";
+            break;
+        case UIDeviceBatteryStateUnknown:
+            self.batteryInfo.status = @"Unknown";
+            break;
+    }
+}
+
 
 @end
