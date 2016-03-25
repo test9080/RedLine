334781a38a836b7fa8e8068fa0ddd6d974448ad8
chengxianghe <chengxianghe@outlook.com>
2016-03-26 00:10:54 +0800

    manager

---
+++ RedLine/Class/Manager/TUSystemInfoManager.m
@@ -0,0 +1,13 @@
+//
+//  TUSystemInfoManager.m
+//  RedLine
+//
+//  Created by chengxianghe on 16/3/26.
+//  Copyright © 2016年 cn. All rights reserved.
+//
+
+#import "TUSystemInfoManager.h"
+
+@implementation TUSystemInfoManager
+
+@end
---
+++ RedLine/Class/UI/Base/TUSystemInfoManager.h
@@ -0,0 +1,13 @@
+//
+//  TUSystemInfoManager.h
+//  RedLine
+//
+//  Created by chengxianghe on 16/3/26.
+//  Copyright © 2016年 cn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TUSystemInfoManager : NSObject
+
+@end
