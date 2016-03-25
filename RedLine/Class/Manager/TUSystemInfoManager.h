d71af135b21fc945eba9dc46f85ff12183a234f4
chengxianghe <chengxianghe@outlook.com>
2016-03-26 04:10:32 +0800

    数据初始化 完成

--- RedLine/Class/Manager/TUSystemInfoManager.h
+++ RedLine/Class/Manager/TUSystemInfoManager.h
@@ -16,16 +16,20 @@
 // 电池信息变化 通知
 #define kBatteryStatusChange @"batteryStatusChange"
 #define kBatteryLevelChange @"batteryLevelChange"
+#define kBatteryInfoChange @"batteryInfoChange"
 
 
 // 电池信息
 @interface BatteryInfo : NSObject
 
 @property (nonatomic, assign) NSUInteger    capacity;
-@property (nonatomic, assign) CGFloat       voltage;
+@property (nonatomic, assign) NSUInteger    voltage; // "Voltage = 4320"
 @property (nonatomic, assign) NSUInteger    levelPercent;
 @property (nonatomic, assign) NSUInteger    levelMAH;
 @property (nonatomic, copy)   NSString      *status;
+@property (nonatomic, assign) NSUInteger    temperature; // "Temperature = 2750"
+@property (nonatomic, assign) NSUInteger    cycleCount; // "CycleCount = 175"
+@property (nonatomic, assign) NSTimeInterval updateTime; // "UpdateTime = 1458934114"
 
 @end
 
