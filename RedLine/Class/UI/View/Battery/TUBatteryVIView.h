b9e24df50d32da3d935724bf4574524f6ffda1f1
li125434104 <125434104@qq.com>
2016-03-28 16:12:54 +0800

    add pod
    add pod

---
+++ RedLine/Class/UI/View/Battery/TUBatteryVIView.h
@@ -0,0 +1,13 @@
+//
+//  TUBatteryVIView.h
+//  RedLine
+//
+//  Created by LXJ on 16/3/28.
+//  Copyright © 2016年 cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TUBatteryVIView : UIView
+
+@end
---
+++ RedLine/Class/UI/View/Battery/TUBatteryVIView.m
@@ -0,0 +1,21 @@
+//
+//  TUBatteryVIView.m
+//  RedLine
+//
+//  Created by LXJ on 16/3/28.
+//  Copyright © 2016年 cn. All rights reserved.
+//
+
+#import "TUBatteryVIView.h"
+
+@implementation TUBatteryVIView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
