015f1be6a91f334e661cc989963a524e90d867e2
chengxianghe <chengxianghe@outlook.com>
2016-03-30 17:41:27 +0800

    time to full

--- RedLine/Class/Manager/TUSystemInfoManager.h
+++ RedLine/Class/Manager/TUSystemInfoManager.h
@@ -14,11 +14,12 @@
 #import <UIKit/UIKit.h>
 
 // 电池信息变化 通知
-#define kBatteryStatusDidChangeNotification    @"batteryStatusChange"
-#define kBatteryLevelDidChangeNotification     @"batteryLevelChange"
-#define kBatteryInfoDidChangeNotification      @"batteryInfoChange"
+#define kBatteryStatusDidChangeNotification             @"batteryStatusChange"
+#define kBatteryLevelDidChangeNotification              @"batteryLevelChange"
+#define kBatteryInfoDidChangeNotification               @"batteryInfoChange"
+#define kBatteryTimeToEmptyOrFullDidChangeNotification  @"batteryInfoChange"
 
-#define kSystemInfoDidChangeNotification       @"systemInfoChange"
+#define kSystemInfoDidChangeNotification                @"systemInfoChange"
 
 
 
@@ -39,6 +40,11 @@
 // 计算数值
 @property (nonatomic, assign) NSInteger remainLifeMonths; // 剩余寿命（月）
 @property (nonatomic,   copy) NSString  *status; // 充电状态
+@property (nonatomic, assign) UIDeviceBatteryState  batteryState; // 充电状态
+
+// 需要监听 kBatteryTimeToEmptyOrFullDidChangeNotification 取值
+@property (nonatomic, assign) NSInteger timeToEmpty; // 用完所需时间（需要电池百分比变化 才有值）
+@property (nonatomic, assign) NSInteger timeToFull; // 充满所需时间（需要电池百分比变化 才有值）
 
 @end
 
@@ -49,6 +55,41 @@
 @property (nonatomic, strong) NSDictionary   *systemInfo; // 包含所有IOKit获取的信息(暂时无数据)
 
 + (TUSystemInfoManager *)manager;
+
+@end
+
+@interface TUSystemInfoManager (Help)
+
+/**
+ *  计算电池充满所需时间
+ *
+ *  @param amperage        平均电流(mA)
+ *  @param maxCapacity     电池最大容量(mAh)
+ *  @param currentCapacity 电池当前容量(mAh)
+ *
+ *  @return 充满所需时间(h)
+ */
++ (CGFloat)timeToFullWithAverageAmperage:(CGFloat)amperage maxCapacity:(CGFloat)maxCapacity currentCapacity:(CGFloat)currentCapacity;
+
+/**
+ *  计算电池剩余可用时间
+ *
+ *  @param amperage         平均电流(mA)
+ *  @param currentCapacity  电池最大容量(mAh)
+ *  @param temperature      电池温度(℃)
+ *
+ *  @return 电池可用时间(h)
+ */
++ (CGFloat)timeToEmptyWithAverageAmperage:(CGFloat)amperage currentCapacity:(CGFloat)currentCapacity temperature:(CGFloat)temperature;
+
+/**
+ *  计算电池剩余寿命
+ *
+ *  @param cycleCount 电池已用循环次数
+ *
+ *  @return 剩余寿命（单位：月）
+ */
++ (NSInteger)getBatteryLifeWithCycleCount:(NSInteger)cycleCount;
 
 @end
 
