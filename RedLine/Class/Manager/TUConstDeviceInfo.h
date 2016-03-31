a6861ee3ea1761598f4ec66e6b93fb16bc2848a5
chengxianghe <chengxianghe@outlook.com>
2016-04-01 00:25:33 +0800

    time

---
+++ RedLine/Class/Manager/TUConstDeviceInfo.h
@@ -0,0 +1,25 @@
+//
+//  TUConstDeviceData.h
+//  RedLine
+//
+//  Created by chengxianghe on 16/3/31.
+//  Copyright © 2016年 cn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TUConstDeviceInfo : NSObject
++ (TUConstDeviceInfo *)sharedDevice;
+
+- (NSString *)getiDeviceName;
+- (const NSString *)getCPUName;
+- (NSUInteger)getCPUFrequency;
+- (const NSString *)getCoprocessorName;
+- (const NSString *)getGraphicCardName;
+- (const NSString *)getRAMType;
+- (NSUInteger)getBatteryCapacity;
+- (CGFloat)getBatteryVoltage;
+- (CGFloat)getScreenSize;
+- (NSUInteger)getPPI;
+- (NSString *)getAspectRatio;
+@end
--- RedLine/Class/Manager/TUSystemInfoManager.h
+++ RedLine/Class/Manager/TUSystemInfoManager.h
@@ -12,46 +12,14 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#import "TUBatteryInfo.h"
 
-// 电池信息变化 通知
-#define kBatteryStatusDidChangeNotification             @"batteryStatusChange"
-#define kBatteryLevelDidChangeNotification              @"batteryLevelChange"
-#define kBatteryInfoDidChangeNotification               @"batteryInfoChange"
-#define kBatteryTimeToEmptyOrFullDidChangeNotification  @"batteryInfoChange"
-
-#define kSystemInfoDidChangeNotification                @"systemInfoChange"
-
-
-
-// 电池信息
-@interface BatteryInfo : NSObject
-
-//@property (nonatomic, assign) NSInteger capacity; // 电池容量 标称 6p 1915
-@property (nonatomic, assign) NSInteger voltage; // 电压mV "Voltage = 4320"
-@property (nonatomic, assign) NSInteger amperage; // 电流mA "InstantAmperage = 137", 负数代表输出
-@property (nonatomic, assign) NSInteger levelPercent; // 电量百分数
-@property (nonatomic, assign) NSInteger temperature; // 电池温度 "Temperature = 2750"
-@property (nonatomic, assign) NSInteger cycleCount; // 电池循环次数 "CycleCount = 175"
-@property (nonatomic, assign) NSInteger rawMaxCapacity; // 电池实际最大容量 "AppleRawMaxCapacity = 2569"
-@property (nonatomic, assign) NSInteger designCapacity; // 电池设计最大容量 "DesignCapacity = 2855"
-@property (nonatomic, assign) NSInteger rawCurrentCapacity; // 电池当前容量 "AppleRawCurrentCapacity = 1576"
-@property (nonatomic, assign) NSInteger updateTime; // 更新时间 "UpdateTime = 1458934114"
-
-// 计算数值
-@property (nonatomic, assign) NSInteger remainLifeMonths; // 剩余寿命（月）
-@property (nonatomic, assign) UIDeviceBatteryState  batteryState; // 充电状态
-@property (nonatomic,   copy) NSString  *status; // 充电状态描述
-
-// 需要监听 kBatteryTimeToEmptyOrFullDidChangeNotification 取值
-@property (nonatomic, assign) NSInteger timeToEmpty; // 用完所需时间（需要电池百分比变化 才有值）
-@property (nonatomic, assign) NSInteger timeToFull; // 充满所需时间（需要电池百分比变化 才有值）
-
-@end
-
+#define kSystemInfoDidReadFinishedNotification          @"systemInfoDidReadFinished"
+#define kSystemBatteryInfoDidReadFinishedNotification   @"systemBatteryInfoDidReadFinished"
 
 @interface TUSystemInfoManager : NSObject
 
-@property (nonatomic, strong) BatteryInfo   *batteryInfo; // 电池信息
+@property (nonatomic, weak) TUBatteryInfo   *batteryInfo; // 电池信息
 @property (nonatomic, strong) NSDictionary   *systemInfo; // 包含所有IOKit获取的信息(暂时无数据)
 
 + (TUSystemInfoManager *)manager;
