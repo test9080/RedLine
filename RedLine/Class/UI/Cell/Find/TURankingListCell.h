68d2f0ca6ba46a2024e6fefbc8c9e99b9fffdd09
li125434104 <125434104@qq.com>
2016-04-21 15:00:25 +0800

    add 排行榜

---
+++ RedLine/Class/UI/Cell/Find/TURankingListCell.h
@@ -0,0 +1,15 @@
+//
+//  TURankingListCell.h
+//  RedLine
+//
+//  Created by LXJ on 16/4/21.
+//  Copyright © 2016年 cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TURankingListCell : UITableViewCell
+
+- (void)setInfo:(BOOL)isSelf;
+
+@end
